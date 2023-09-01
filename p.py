34b3ea6299e1093d4a0e16d6d2c85da83b4dbd90
Sonywahyupratama <142053324+Sonywahyupratama@users.noreply.github.com>
2023-09-02 05:38:08 +0700

    Update p.py

--- p.py
+++ p.py
@@ -1,49 +1,35 @@
-import asyncio
 import aiohttp
-import threading
+import asyncio
 
-async def fetch_url(url):
+async def send_request(url):
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 response_text = await response.text()
-                if response.status == 200:
-                    return "Sukses"
-                else:
-                    return f"Kesalahan: Kode status {response.status}"
+                return response_text
     except Exception as e:
-        return "Kesalahan"
+        return None
 
-def fetch_in_thread(url, num_requests):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    responses = loop.run_until_complete(fetch_multiple_urls(url, num_requests))
-    loop.close()
-    return responses
-
-async def fetch_multiple_urls(url, num_requests):
-    tasks = []
-    for _ in range(num_requests):
-        task = asyncio.ensure_future(fetch_url(url))
-        tasks.append(task)
-    return await asyncio.gather(*tasks)
-
-def main():
-    url = "https://sxtcp.tg-index.workers.dev"
-    num_requests = 100
-    num_threads = 100
+async def main():
+    default_url = 'https://sxtcp.tg-index.workers.dev'  # URL default
+    num_requests = 1000 # Jumlah permintaan yang ingin Anda kirim
 
     while True:
-        print("Program berjalan...")
-        thread_list = []
+        # Inisialisasi list untuk menyimpan hasil respons
+        responses = []
 
-        for _ in range(num_threads):
-            thread = threading.Thread(target=fetch_in_thread, args=(url, num_requests))
-            thread_list.append(thread)
-            thread.start()
+        # Buat daftar 10 tugas untuk mengirim permintaan
+        tasks = [send_request(default_url) for _ in range(num_requests)]
 
-        for thread in thread_list:
-            thread.join()
+        # Jalankan tugas-tugas secara bersamaan
+        responses = await asyncio.gather(*tasks)
+
+        # Handle hasil respons di sini
+        for response in responses:
+            if response is not None:
+                print('Success')
+            else:
+                print('Failed')
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
